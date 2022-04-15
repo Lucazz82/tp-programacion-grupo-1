cd1b110c3b2f8303d11c77c8756919a9a76ee55b
Teoramites <teoramites@gmail.com>
2022-04-15 18:30:25 -0300

    AGENCIA SINGLETON

--- src/modelos/Agencia.java
+++ src/modelos/Agencia.java
@@ -2,4 +2,11 @@ package modelos;
 
 public class Agencia {
 	// Singleton!1!1!
+	private static Agencia _instancia=null;
+	public static Agencia getInstancia() {
+		if (_instancia == null)
+			_instancia=new Agencia();
+		return _instancia;
+	}
+	
 }
