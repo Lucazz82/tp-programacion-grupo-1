204dbbef565c1ee509692b4eecbbf306e3fb14d4
Teoramites <teoramites@gmail.com>
2022-04-15 18:45:44 -0300

    ticket gral

--- src/modelos/Ticket.java
+++ src/modelos/Ticket.java
@@ -1,5 +1,21 @@
 package modelos;
 
-public abstract class Ticket {
+import java.util.Date;
 
+import enums.EstadosTicket;
+
+public abstract class Ticket {
+	protected Date fechaAlta;
+	protected EstadosTicket estado;
+	public Ticket() {
+		this.fechaAlta=new Date();
+		this.estado=EstadosTicket.ACTIVO;
+	}
+	public EstadosTicket getEstado() {
+		return estado;
+	}
+	public void setEstado(EstadosTicket estado) {
+		this.estado = estado;
+	}
+	
 }
