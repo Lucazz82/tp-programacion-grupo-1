752a70c68bcff51abaf8baf251b889000102ef7d
Renzo Romeo <renzoromeo44@gmail.com>
2022-04-15 18:28:46 -0300

    usuario

--- src/modelos/Usuario.java
+++ src/modelos/Usuario.java
@@ -1,5 +1,25 @@
 package modelos;
 
 public abstract class Usuario {
+	private String nombre;
+	private String contrasena;
+	private int puntaje;
+	
+	public Usuario(String nombre, String contrasena) {
+		this.nombre = nombre;
+		this.contrasena = contrasena;
+		this.puntaje = 0;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public int getPuntaje() {
+		return puntaje;
+	}
 }
