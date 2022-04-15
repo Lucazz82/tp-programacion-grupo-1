9f5192277f14dc922db89c485756f37caedf026a
Renzo Romeo <renzoromeo44@gmail.com>
2022-04-15 18:53:10 -0300

    excepciones

---
+++ src/excepciones/UsuarioInexistenteException.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+public class UsuarioInexistenteException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public UsuarioInexistenteException(String mensaje) {
+		super(mensaje);
+	}
+}
