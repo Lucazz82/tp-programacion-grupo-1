d2a8f3ed4c5e56805740dfd8c2d3ac70527220f4
camicacace <102537508+camicacace@users.noreply.github.com>
2022-04-21 12:28:54 -0300

    correccion

--- src/modelos/aspectos/Remuneracion.java
+++ src/modelos/aspectos/Remuneracion.java
@@ -1,13 +1,9 @@
 package modelos.aspectos;
 
 public class Remuneracion {
-
-<<<<<<< HEAD
 	private static double[][] pesos = {{1, -0.5, -1}, {1, 1, -0.5}, {1, 1, 1}};
 	
 	public static double enfrentar(int remuneracionRequerida, int remuneracionConseguida) {
 		return pesos[remuneracionRequerida][remuneracionConseguida];
 	}	
-=======
->>>>>>> f51f20ddb6798ebeeedf3dc4a6be7801d4944a5a
 }
