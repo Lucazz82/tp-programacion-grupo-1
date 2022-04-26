36ee1798a69fc9a8465c03fcf556826b4b8c5052
Renzo Romeo <renzoromeo44@gmail.com>
2022-04-26 12:16:04 -0300

    Experiencia factory

--- src/modelos/aspectos/factories/ExperienciaFactory.java
+++ src/modelos/aspectos/factories/ExperienciaFactory.java
@@ -1,4 +1,22 @@
 package modelos.aspectos.factories;
 
+import enums.Experiencias;
+import modelos.aspectos.Experiencia;
+
 public class ExperienciaFactory {
+	public static Experiencia getExperiencia(Experiencias experiencia) {
+		Experiencia resultado = null;
+		switch (experiencia) {
+		case MEDIA:
+			resultado = new Experiencia(1);
+			break;
+		case MUCHA:
+			resultado = new Experiencia(2);
+			break;
+		case NADA:
+			resultado = new Experiencia(0);
+			break;
+		}
+		return resultado;
+	}
 }
