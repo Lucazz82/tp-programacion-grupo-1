25f9ffa481956ffd4dd353988df3b8ce082209a6
Teoramites <teoramites@gmail.com>
2022-06-07 11:11:37 -0300

    AgenciaDTOfromAgencia

--- src/persistencia/UtilDTO.java
+++ src/persistencia/UtilDTO.java
@@ -1,8 +1,17 @@
 package persistencia;
 
+import modelos.Agencia;
+
 public class UtilDTO {
 	public AgenciaDTO agenciaDTOFromAgencia() {
+		AgenciaDTO respuesta = new AgenciaDTO();
+		respuesta.setCoincidencias(Agencia.getInstancia().getCoincidencias());
+		respuesta.setEmpleadores(Agencia.getInstancia().getEmpleadores());
+		respuesta.setEmpleados(Agencia.getInstancia().getEmpleados());
+		respuesta.setFechaLista(Agencia.getInstancia().getFechaLista()); //ESTE NO SE PUEDE PERSISTIR ES GREGORIAN CALENDAR
+		respuesta.setListasAsignaciones(Agencia.getInstancia().getListasAsignaciones());
 		
+		return respuesta;
 	}
 	
 	public void agenciaFromAgenciaDTO(AgenciaDTO agenciaDTO) {
