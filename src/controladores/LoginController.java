10af18c4759552fa607d09c54d7d7c04dc9a2a68
Lucas <lucasdelellis2002@gmail.com>
2022-06-25 17:02:56 -0300

    Refactor login controller

--- src/controladores/LoginController.java
+++ src/controladores/LoginController.java
@@ -44,43 +44,4 @@ public class LoginController extends Controller {
 			vista.setVisible(false);
 		}
 	}
-
-	@Override
-	public void windowOpened(WindowEvent e) {
-		
-	}
-
-	@Override
-	public void windowClosing(WindowEvent e) {
-		Sistema.getInstancia().cerrarSistema();
-	}
-
-	@Override
-	public void windowClosed(WindowEvent e) {		
-	}
-
-	@Override
-	public void windowIconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void windowDeiconified(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void windowActivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void windowDeactivated(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
