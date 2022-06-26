de8179435d7a59d23668cbc7ea2a2608a38affb8
Lucas <lucasdelellis2002@gmail.com>
2022-06-26 20:39:57 -0300

    toString puesto laboral

--- src/modelos/aspectos/PuestoLaboral.java
+++ src/modelos/aspectos/PuestoLaboral.java
@@ -2,12 +2,15 @@ package modelos.aspectos;
 
 import java.io.Serializable;
 
+import enums.PuestosLaborales;
 import modelos.Enfrentable;
 
 public class PuestoLaboral implements Enfrentable<PuestoLaboral>, Serializable {
+	private PuestosLaborales tipo;
 	private int pos;
 
-	public PuestoLaboral(int pos) {
+	public PuestoLaboral(PuestosLaborales tipo, int pos) {
+		this.tipo = tipo;
 		this.pos = pos;
 	}
 
@@ -15,6 +18,11 @@ public class PuestoLaboral implements Enfrentable<PuestoLaboral>, Serializable {
 
 	public double enfrentar(PuestoLaboral o) {
 		return pesos[this.pos][o.pos];
+	}
+	
+	@Override
+	public String toString() {
+		return tipo.name();
 	}
 
 }
