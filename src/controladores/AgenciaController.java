470d4d89b37f7a5f6d436efccbef0b52a751a98f
Lucas <lucasdelellis2002@gmail.com>
2022-06-26 16:17:18 -0300

    Agencia controller

--- src/controladores/AgenciaController.java
+++ src/controladores/AgenciaController.java
@@ -1,12 +1,58 @@
 package controladores;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import excepciones.AgenciaInexistenteException;
 import modelos.Agencia;
+import modelos.Usuario;
+import vista.AgenciaVista;
 
-public class AgenciaController {
-	private Agencia agencia = Agencia.getInstancia();
-
-	public void generaListasAsignacion() {
-		agencia.generarListasAsignacion();
-		agencia.calcularPuntajes();
+public class AgenciaController extends Controller {
+	private AgenciaVista vista;
+	
+	public AgenciaController() {
+		this.vista = new AgenciaVista();
+		this.vista.setActionListener(this);
+		this.setListaUsuarios();
 	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Agencia agencia = null;
+		try {
+			agencia = Agencia.getInstancia();
+		} catch (AgenciaInexistenteException e1) {}
+		
+		if(e.getActionCommand().equalsIgnoreCase("Cerrar Sesion")) {
+			Sistema.getInstancia().cambiarController(new LoginController());
+			this.vista.setVisible(false);
+		} else if(e.getActionCommand().equalsIgnoreCase("Generar Listas de Asignacion")) {
+			agencia.generarListasAsignacion();
+			JOptionPane.showMessageDialog(vista, "Listas de asignacion generadas con exito");
+		} else if(e.getActionCommand().equalsIgnoreCase("Ver Listas de Asignacion")) {
+			
+		} else if(e.getActionCommand().equalsIgnoreCase("Iniciar Ronda de Contratacion")) {
+			agencia.rondaContratacion();
+			JOptionPane.showMessageDialog(vista, "Ronda de contratacion generada con exito");
+		} else if(e.getActionCommand().equalsIgnoreCase("Ver Coincidencias")) {
+			
+		}
+	}
+	
+	private void setListaUsuarios() {
+		Agencia agencia = null;
+		try {
+			 agencia = Agencia.getInstancia();
+		} catch (AgenciaInexistenteException e) {}
+		
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.addAll(agencia.getEmpleadores());
+		usuarios.addAll(agencia.getEmpleados());
+		
+		this.vista.setListaUsuarios(usuarios);
+	}
+
 }
